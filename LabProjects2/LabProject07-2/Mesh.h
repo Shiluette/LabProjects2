b90cd7171a3d8114cd22bdc7b8495b51adc795ea
kimyongkwon <dydrnjs518@naver.com>
2017-01-07 23:57:08 +0900

    lab072-Camera

---
+++ LabProjects2/LabProject07-2/Mesh.h
@@ -0,0 +1,103 @@
+#pragma once
+
+class CVertex
+{
+	D3DXVECTOR3 m_d3dxvPosition;
+public:
+	CVertex() { m_d3dxvPosition = D3DXVECTOR3(0, 0, 0); }
+	CVertex(D3DXVECTOR3 d3dxvPosition) { m_d3dxvPosition = d3dxvPosition; }
+	~CVertex() { }
+};
+
+//052
+class CDiffusedVertex
+{
+	D3DXVECTOR3 m_d3dxvPosition;
+	D3DXCOLOR m_d3dxcDiffuse;
+public:
+	//생성자와 소멸자를 선언한다.
+	CDiffusedVertex(float x, float y, float z, D3DXCOLOR d3dxcDiffuse) { m_d3dxvPosition = D3DXVECTOR3(x, y, z); m_d3dxcDiffuse = d3dxcDiffuse; }
+	CDiffusedVertex(D3DXVECTOR3 d3dxvPosition, D3DXCOLOR d3dxcDiffuse) { m_d3dxvPosition = d3dxvPosition; m_d3dxcDiffuse = d3dxcDiffuse; }
+	CDiffusedVertex() { m_d3dxvPosition = D3DXVECTOR3(0.0f, 0.0f, 0.0f); m_d3dxcDiffuse = D3DXCOLOR(0.0f, 0.0f, 0.0f, 0.0f); }
+	~CDiffusedVertex() { }
+};
+
+class CMesh
+{	
+	//CMesh 클래스 객체의 참조(Reference)와 관련된 멤버 변수와 함수를 선언한다.
+private:
+	int m_nReferences;
+	
+protected:
+	//05-1
+	/*래스터라이저 상태 객체에 대한 인터페이스 포인터를 선언한다. 래스터라이저 상태 객체는 “CGameObject" 클래스의 멤버로 추가하여도 된다. 래스터라이저 상태 객체가 메쉬의 멤버일 때와 게임 객체의 멤버일 때의 의미상의 차이를 생각해보기를 바란다.*/
+	ID3D11RasterizerState *m_pd3dRasterizerState;
+
+	//정점 데이터를 저장하기 위한 정점 버퍼 인터페이스 포인터를 선언한다.
+	ID3D11Buffer *m_pd3dVertexBuffer;
+
+	/*정점 버퍼의 정점 개수, 정점의 바이트 수, 정점 데이터가 정점 버퍼의 어디에서부터 시작하는 가를 나타내는 변수를 선언한다.*/
+	UINT m_nVertices;
+	UINT m_nStride;
+	UINT m_nOffset;
+
+	//정점 데이터가 어떤 프리미티브를 표현하고 있는 가를 나타내는 멤버 변수를 선언한다.
+	D3D11_PRIMITIVE_TOPOLOGY m_d3dPrimitiveTopology;
+
+public:
+	CMesh(ID3D11Device *pd3dDevice);
+	virtual ~CMesh();
+
+	void AddRef();
+	void Release();
+
+	//정점 데이터를 렌더링하는 멤버 함수를 선언한다.
+	virtual void Render(ID3D11DeviceContext *pd3dDeviceContext);
+
+	//05-1
+	virtual void CreateRasterizerState(ID3D11Device *pd3dDevice);
+
+	//--------------------------------------------------------------------------------
+	//07 (내 생각에는 아래내용은 private로 바뀌어야할것같다)
+	/*인덱스 버퍼(인덱스의 배열)에 대한 인터페이스 포인터이다. 인덱스 버퍼는 정점 버퍼(배열)에 대한 인덱스를 가진다.*/
+	ID3D11Buffer *m_pd3dIndexBuffer;
+	//인덱스 버퍼가 포함하는 인덱스의 개수이다. 
+	UINT m_nIndices;
+	//인덱스 버퍼에서 메쉬를 표현하기 위해 사용되는 시작 인덱스이다. 
+	UINT m_nStartIndex;
+	//각 인덱스에 더해질 인덱스이다. 
+	int m_nBaseVertex;
+	//--------------------------------------------------------------------------------
+};
+
+class CTriangleMesh : public CMesh
+{
+public:
+	CTriangleMesh(ID3D11Device *pd3dDevice);
+	virtual ~CTriangleMesh();
+
+	virtual void Render(ID3D11DeviceContext *pd3dDeviceContext);
+
+	//05-1
+	virtual void CreateRasterizerState(ID3D11Device *pd3dDevice);
+};
+
+//06
+class CCubeMesh : public CMesh
+{
+public:
+	//072
+	CCubeMesh(ID3D11Device *pd3dDevice, float fWidth = 2.0f, float fHeight = 2.0f, float fDepth = 2.0f, D3DXCOLOR d3dxColor = D3DXCOLOR(1.0f, 1.0f, 0.0f, 0.0f));
+	virtual ~CCubeMesh();
+
+	virtual void CreateRasterizerState(ID3D11Device *pd3dDevice);
+	virtual void Render(ID3D11DeviceContext *pd3dDeviceContext);
+};
+
+//072
+class CAirplaneMesh : public CMesh
+{
+public:
+	CAirplaneMesh(ID3D11Device *pd3dDevice, float fWidth = 20.0f, float fHeight = 20.0f, float fDepth = 4.0f, D3DXCOLOR d3dxColor = D3DXCOLOR(1.0f, 1.0f, 0.0f, 0.0f));
+	virtual ~CAirplaneMesh();
+};
