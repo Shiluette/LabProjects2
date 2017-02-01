6bcb60b8f536c524862fd57c7062753e3cdf9721
kimyongkwon <dydrnjs518@naver.com>
2017-02-01 19:43:44 +0900

    lab130-texture(TerrainTexture)

---
+++ LabProjects2/LabProject13-0/Shader.h
@@ -0,0 +1,167 @@
+#pragma once
+
+#include "Object.h"
+#include "Camera.h"
+#include "Player.h"
+
+struct VS_CB_WORLD_MATRIX
+{
+	D3DXMATRIX m_d3dxmtxWorld;
+};
+/*인스턴싱 데이터를 나타내는 구조체 VS_VB_INSTANCE를 다음과 같이 선언*/
+struct VS_VB_INSTANCE
+{
+	D3DXMATRIX m_d3dxTransform;
+	D3DXCOLOR m_d3dxColor;
+};
+
+class CShader
+{
+private:
+	int m_nReferences;
+
+public:
+	void AddRef() { m_nReferences++; }
+	void Release() { if (--m_nReferences <= 0) delete this; }
+
+public:
+	
+	CShader();
+	virtual ~CShader();
+
+	void CreateVertexShaderFromFile(ID3D11Device *pd3dDevice, WCHAR *pszFileName, LPCSTR pszShaderName, LPCSTR pszShaderModel, ID3D11VertexShader **ppd3dVertexShader, D3D11_INPUT_ELEMENT_DESC *pd3dInputLayout, UINT nElements, ID3D11InputLayout **ppd3dVertexLayout);
+	void CreatePixelShaderFromFile(ID3D11Device *pd3dDevice, WCHAR *pszFileName, LPCSTR pszShaderName, LPCSTR pszShaderModel, ID3D11PixelShader **ppd3dPixelShader);
+
+	virtual void CreateShader(ID3D11Device *pd3dDevice);
+
+	static void CreateShaderVariables(ID3D11Device *pd3dDevice);
+	static void ReleaseShaderVariables();
+	static void UpdateShaderVariable(ID3D11DeviceContext *pd3dDeviceContext, D3DXMATRIX *pd3dxmtxWorld);
+
+	//게임 객체들을 생성하고 애니메이션 처리를 하고 렌더링하기 위한 함수이다.
+	virtual void BuildObjects(ID3D11Device *pd3dDevice);
+	virtual void ReleaseObjects();
+	virtual void AnimateObjects(float fTimeElapsed);
+	virtual void OnPrepareRender(ID3D11DeviceContext *pd3dDeviceContext);
+	virtual void Render(ID3D11DeviceContext *pd3dDeviceContext, CCamera *pCamera = NULL);
+
+protected:
+	ID3D11VertexShader *m_pd3dVertexShader;
+	ID3D11InputLayout *m_pd3dVertexLayout;
+
+	ID3D11PixelShader *m_pd3dPixelShader;
+
+	//쉐이더 객체가 게임 객체들의 리스트를 가진다.
+	CGameObject **m_ppObjects;
+	int m_nObjects;
+
+	//월드 변환 행렬을 위한 상수 버퍼는 하나만 있어도 되므로 정적 멤버로 선언한다.
+	static ID3D11Buffer *m_pd3dcbWorldMatrix;
+
+public:
+	virtual CGameObject *PickObjectByRayIntersection(D3DXVECTOR3 *pd3dxvPickPosition, 
+		D3DXMATRIX *pd3dxmtxView, MESHINTERSECTINFO *pd3dxIntersectInfo);
+};
+
+class CDiffusedShader : public CShader
+{
+public:
+	CDiffusedShader();
+	virtual ~CDiffusedShader();
+
+	virtual void CreateShader(ID3D11Device *pd3dDevice);
+};
+
+//게임 객체들을 렌더링하기 위한 쉐이더 클래스이다.
+class CSceneShader : public CShader
+{
+public:
+	CSceneShader();
+	virtual ~CSceneShader();
+
+	virtual void CreateShader(ID3D11Device *pd3dDevice);
+	virtual void BuildObjects(ID3D11Device *pd3dDevice);
+};
+
+class CIlluminatedShader : public CShader
+{
+public:
+	CIlluminatedShader();
+	virtual ~CIlluminatedShader();
+
+	virtual void CreateShader(ID3D11Device *pd3dDevice);
+
+	//재질을 설정하기 위한 상수 버퍼이다.
+	static ID3D11Buffer	 *m_pd3dcbMaterial;
+
+	static void CreateShaderVariables(ID3D11Device *pd3dDevice);
+	static void ReleaseShaderVariables();
+	//재질을 쉐이더 변수에 설정(연결)하기 위한 함수이다.
+	static void UpdateShaderVariable(ID3D11DeviceContext *pd3dDeviceContext, MATERIAL *pMaterial);
+};
+
+class CTexturedShader : public CShader
+{
+public:
+	CTexturedShader();
+	virtual ~CTexturedShader();
+
+	virtual void CreateShader(ID3D11Device *pd3dDevice);
+};
+
+//플레이어를 렌더링하기 위한 쉐이더 클래스이다.
+class CPlayerShader : public CIlluminatedShader
+{
+public:
+	CPlayerShader();
+	virtual ~CPlayerShader();
+
+	virtual void CreateShader(ID3D11Device *pd3dDevice);
+	virtual void BuildObjects(ID3D11Device *pd3dDevice);
+	virtual void Render(ID3D11DeviceContext *pd3dDeviceContext, CCamera *pCamera = NULL);
+
+	CPlayer *GetPlayer(int nIndex = 0) { return((CPlayer *)m_ppObjects[nIndex]); }
+};
+
+//CInstancingShader 클래스의 베이스 클래스를 CIlluminatedShader로 변경한다.
+class CInstancingShader : public CTexturedShader
+{
+
+private:
+	CMaterial *m_pMaterial;
+	CTexture *m_pTexture;
+public:
+	CInstancingShader();
+	virtual ~CInstancingShader();
+
+	virtual void CreateShader(ID3D11Device *pd3dDevice);
+
+	virtual void Render(ID3D11DeviceContext *pd3dDeviceContext, CCamera *pCamera);
+
+	virtual void BuildObjects(ID3D11Device *pd3dDevice, CHeightMapTerrain *pHeightMapTerrain,
+		CMaterial *pMaterial, CTexture *pTexture, int k);
+private:
+	UINT m_nInstanceBufferStride;
+	UINT m_nInstanceBufferOffset;
+
+	ID3D11Buffer *m_pd3dCubeInstanceBuffer;
+	ID3D11Buffer *m_pd3dSphereInstanceBuffer;
+
+public:
+	ID3D11Buffer *CreateInstanceBuffer(ID3D11Device *pd3dDevice, 
+		int nObjects, UINT nBufferStride, void *pBufferData);
+
+};
+
+//130
+class CTerrainShader : public CTexturedShader
+{
+public:
+	CTerrainShader();
+	virtual ~CTerrainShader();
+
+	virtual void BuildObjects(ID3D11Device *pd3dDevice);
+
+	CHeightMapTerrain *GetTerrain();
+};
+
